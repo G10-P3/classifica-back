5f3e4fdecbdadf35428f0cca0f73b82ecdf455bf
Gheyson Melo <gheysonmelo@hotmail.com>
2024-09-30 20:37:34 -0300

    First Commit

---
+++ src/main/java/br/com/g10/BEM/BemApplication.java
@@ -0,0 +1,13 @@
+package br.com.g10.BEM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/g10/BEM/BemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.g10.BEM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
