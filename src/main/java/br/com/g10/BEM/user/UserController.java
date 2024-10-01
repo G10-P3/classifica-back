a607204b0b21fe8684cbff61406e98c0ea5a83f1
Gheyson Melo <gheysonmelo@hotmail.com>
2024-09-30 22:34:35 -0300

    add: IUserRepository and createuser endpoint

--- src/main/java/br/com/g10/BEM/user/UserController.java
+++ src/main/java/br/com/g10/BEM/user/UserController.java
@@ -3,6 +3,9 @@ package br.com.g10.BEM.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -10,10 +13,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void createUser(@RequestBody UserModel userModel) {
+    public ResponseEntity createUser(@RequestBody UserModel userModel) {
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            // Retornar mensagem de erro + status code 400
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+        }
+
         // Create a new user
-        System.out.println(userModel.getUsername());
+        var userCreated = this.userRepository.save(userModel);
+
+        // Return the user created
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
     
 }
