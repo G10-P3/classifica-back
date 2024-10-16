4cd6006f873e66a0299896e3c4ab54301f62fbc1
Gheyson Melo <gheysonmelo@hotmail.com>
2024-10-15 22:19:33 -0300

    add: checkstyle plugin

--- src/main/java/br/com/g10/BEM/user/UserController.java
+++ src/main/java/br/com/g10/BEM/user/UserController.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @SuppressWarnings("rawtypes")
 @RestController
@@ -23,15 +20,15 @@ public class UserController {
     @PostMapping("/")
     public ResponseEntity createUser(@RequestBody UserModel userModel) {
 
-        var user = this.userRepository.findByUsername(userModel.getUsername());
+        final var user = this.userRepository.findByUsername(userModel.getUsername());
 
-        if(user != null) {
+        if (user != null) {
             // Retornar mensagem de erro + status code 400
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
         }
 
         // Create a new user
-        var userCreated = this.userRepository.save(userModel);
+        final var userCreated = this.userRepository.save(userModel);
 
         // Return the user created
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
